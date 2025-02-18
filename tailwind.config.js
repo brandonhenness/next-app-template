9a931e149137766dd20484e65f73dcc3ace2caec
Brandon Henness <Henness0666@gmail.com>
2025-02-17 16:11:07 -0800

    chore: restructure project files and update configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-import {heroui} from "@heroui/theme"
+import { heroui } from "@heroui/theme"
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
